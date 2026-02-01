a41928aea1d2eee1b0ac45b4e57dd476af78d165
michal.czopek <czopek920@gmail.com>
2026-02-01 19:21:13 +0100

    End of section 16

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51SQvL0CoXK274EEdYtBF0w1ksqoOOBcMOGs3RJd2DAWsSM4Owwf19I2b2GXV3l3II75R3PVksfeZlIuAfUQqdiMR00pkOipD4S'
 };
