423002aa9929884ffd25364fb9f30c192a8fdf4e
michal.czopek <czopek920@gmail.com>
2026-02-08 20:49:11 +0100

    End of section 18

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51SQvL0CoXK274EEdYtBF0w1ksqoOOBcMOGs3RJd2DAWsSM4Owwf19I2b2GXV3l3II75R3PVksfeZlIuAfUQqdiMR00pkOipD4S'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51SQvL0CoXK274EEdYtBF0w1ksqoOOBcMOGs3RJd2DAWsSM4Owwf19I2b2GXV3l3II75R3PVksfeZlIuAfUQqdiMR00pkOipD4S'
 };
